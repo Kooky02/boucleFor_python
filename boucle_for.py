9f417fca5e700c3e088672ea2de480e48bc4bdad
wisner <wisner@prod-sys>
2019-05-31 13:43:49 +0200

    Ajout fichier boucle_for.py

---
+++ boucle_for.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.7
+# -*-coding:Utf-8 -*
+
+
+#Utilisation du boucle for
+
+#Boucle permettant d'enlever un nombre allant de 0 à 10 au carré
+
+for i in range(10):
+    print(i, "*", i, "=", i**2)
